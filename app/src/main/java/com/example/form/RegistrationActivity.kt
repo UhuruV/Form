0c03d2f7ed5e4e9cf73ed2461d1a0d285588c425
Mary Bahati <bahatimary745@gmail.com>
2020-07-27 19:00:51 +0300

    Capture user input from the fields and also add icons to layouts.Added registration layout and intent.

--- app/src/main/java/com/example/form/RegistrationActivity.kt
+++ app/src/main/java/com/example/form/RegistrationActivity.kt
@@ -2,10 +2,23 @@ package com.example.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        regButton.setOnClickListener {
+            var name = etName.text.toString()
+            var email = etEmailAddres.text.toString()
+            var phoneNumber = etPNnumber.text.toString()
+            var password = etPassword.text.toString()
+            var confirm = etConfirmPassword.text.toString()
+            Toast.makeText(baseContext, password, Toast.LENGTH_LONG).show()
+            Toast.makeText(baseContext, confirm, Toast.LENGTH_LONG).show()
+            Toast.makeText(baseContext, email, Toast.LENGTH_LONG).show()
+        }
     }
 }
